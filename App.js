c64e69f45accd309030775265bd9ec9db404024f
sivaprasad1358 <sivaprasadtelukutla@gmail.com>
2025-06-20 10:05:44 +0530

    episode 2 10:05:

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDom from "react-dom";
 const headingEl = React.createElement("h2", { className: "heading" }, "Hello World");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(headingEl);
---
+++ dist/Namaste-React.6bd02f5a.js
@@ -0,0 +1,7 @@
+const headingEl = React.createElement("h2", {
+    className: "heading"
+}, "Hello World");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(headingEl);
+
+//# sourceMappingURL=Namaste-React.6bd02f5a.js.map
